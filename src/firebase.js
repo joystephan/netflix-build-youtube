90c3e39bd33773455ce5534b510180a2ce4d3dfd
joystephan <jestephan931@gmail.com>
2023-08-11 17:36:14 +0300

    Netflix-clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvn66MhzuzG79qowv0xDf5ywAaiLnRPUs",
+    authDomain: "netflix-clone-ffb64.firebaseapp.com",
+    projectId: "netflix-clone-ffb64",
+    storageBucket: "netflix-clone-ffb64.appspot.com",
+    messagingSenderId: "171718957177",
+    appId: "1:171718957177:web:7408ae874d95c550849ca8"
+  };
